000aaf84a1fc34181265ad65c78b1ed4560ea7b5
Alan Denison and James Howard <james@>
2016-06-01 16:17:26 -0700

    created survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
+
+
+
 $(document).ready(function() {
+  $("button#submit").click(function(event) {
+    var continent = $("#continent").val();
+    var happy = $("input:radio[name=happy]:checked").val();
+    var color = $("#color").val();
+    var final = "results. You are from " + continent + ". You are " + happy + "happy with your continent, and the color of your continent is:" + color;
+
+    alert(final);
+    event.preventDefault()
+  })
 });
